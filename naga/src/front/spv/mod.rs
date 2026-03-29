2a2b245927d8ab3a89e314fb8a767a6e8c054c83
JMS55 <47158642+JMS55@users.noreply.github.com>
2026-03-29 14:52:21 -0400

    Patch spriv statements

--- naga/src/front/spv/mod.rs
+++ naga/src/front/spv/mod.rs
@@ -1618,6 +1618,26 @@ impl<I: Iterator<Item = u32>> Frontend<I> {
                     self.patch_statements(body, expressions, fun_parameter_sampling)?;
                     self.patch_statements(continuing, expressions, fun_parameter_sampling)?;
                 }
+                S::ForLoop {
+                    ref mut initializer,
+                    condition: _,
+                    ref mut condition_block,
+                    ref mut update,
+                    ref mut body,
+                } => {
+                    self.patch_statements(initializer, expressions, fun_parameter_sampling)?;
+                    self.patch_statements(condition_block, expressions, fun_parameter_sampling)?;
+                    self.patch_statements(update, expressions, fun_parameter_sampling)?;
+                    self.patch_statements(body, expressions, fun_parameter_sampling)?;
+                }
+                S::WhileLoop {
+                    condition: _,
+                    ref mut condition_block,
+                    ref mut body,
+                } => {
+                    self.patch_statements(condition_block, expressions, fun_parameter_sampling)?;
+                    self.patch_statements(body, expressions, fun_parameter_sampling)?;
+                }
                 S::Break
                 | S::Continue
                 | S::Return { .. }
@@ -1632,9 +1652,7 @@ impl<I: Iterator<Item = u32>> Frontend<I> {
                 | S::SubgroupBallot { .. }
                 | S::SubgroupCollectiveOperation { .. }
                 | S::SubgroupGather { .. }
-                | S::RayPipelineFunction(..)
-                | S::ForLoop { .. }
-                | S::WhileLoop { .. } => {}
+                | S::RayPipelineFunction(..) => {}
                 S::Call {
                     function: ref mut callee,
                     ref arguments,
